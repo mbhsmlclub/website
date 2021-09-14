0375b678b7d4eedbb17d02f5e9e33fafdeb66b2d
Vijay Shanmugam <vijay@Vijays-MacBook-Air.local>
2021-09-14 11:55:37 -0400

    updated areeb photo and added labels

--- src/pages/About/Team.js
+++ src/pages/About/Team.js
@@ -4,6 +4,7 @@ import Hero from 'pages/Hero';
 import AreebIMG from '../../assets/areeb.jpg';
 import MichaelIMG from '../../assets/michael.png';
 import VijayIMG from '../../assets/vijay.jpg';
+import Anchor from '../../components/Anchor'
 import './Team.css'
 
 function Team(props) {
@@ -16,21 +17,36 @@ function Team(props) {
       {...props}
     />
     <div className='captainPhotos'>
+      <div>
       <a href='https://github.com/Qwerty71'>
         <Tilt glareEnable={true} glareMaxOpacity={0.8} tiltMaxAngleX={5} tiltMaxAngleY={5} glareColor="#ffffff" glarePosition="bottom">
           <img src={AreebIMG} alt="Areeb Gani" width='450' height='450'/>
         </Tilt>
       </a>
+      <a className="member__name" href='https://github.com/Qwerty71'>
+          Areeb gani
+      </a>
+      </div>
+      <div>
       <a href='https://mci.sh/'>
         <Tilt glareEnable={true} glareMaxOpacity={0.8} tiltMaxAngleX={5} tiltMaxAngleY={5} glareColor="#ffffff" glarePosition="bottom">
           <img src={MichaelIMG} alt="Michael Ilie" width='450' height='450'/>
         </Tilt>
       </a>
+      <a className="member__name" href='https://mci.sh/'>
+          Michael Ilie
+      </a>
+      </div>
+      <div>
       <a href='https://www.vijayrs.ml/'>
         <Tilt glareEnable={true} glareMaxOpacity={0.8} tiltMaxAngleX={5} tiltMaxAngleY={5} glareColor="#ffffff" glarePosition="bottom">
           <img src={VijayIMG} alt="Vijay Shanmugam" width='450' height='450'/>
         </Tilt>
       </a>
+      <a className="member__name" href='https://www.vijayrs.ml/'>
+          Vijay Shanmugam
+      </a>
+      </div>
     </div>
     </Fragment>
   );
